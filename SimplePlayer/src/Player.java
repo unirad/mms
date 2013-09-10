3487272dfc57083830e155b59e21f35646152be8
nash-pwnage <naveesh.bajpai@gmail.com>
2013-09-10 04:31:24 -0500

    first comment added naveesh.

--- SimplePlayer/src/Player.java
+++ SimplePlayer/src/Player.java
@@ -5,5 +5,6 @@ public class Player {
 	//and play it like a media player. We will integrate pplay pause and stop as well
 	//key goal of this class will be to learn about the progress bar usage
 	//and learn how to see the time corresponding to the frame of a song being played.
-	
+
+	//naveesh added comments
 }
