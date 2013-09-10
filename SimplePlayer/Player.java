ab4d1452d1cd1a78f22900404a8d114ee5f274e8
theblackprince89 <tbproy@gmail.com>
2013-09-10 04:14:10 -0500

    updated project goal

--- SimplePlayer/Player.java
+++
@@ -1,8 +0,0 @@
-
-public class Player {
-
-  public static void main(String inputs[])
-  {
-	  System.out.println("Hello");
-  }
-}
--- SimplePlayer/src/Player.java
+++ SimplePlayer/src/Player.java
@@ -1,4 +1,9 @@
 
 public class Player {
 
+	//this class will be able to accept a single or list of files 
+	//and play it like a media player. We will integrate pplay pause and stop as well
+	//key goal of this class will be to learn about the progress bar usage
+	//and learn how to see the time corresponding to the frame of a song being played.
+	
 }
