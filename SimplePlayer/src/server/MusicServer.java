ffd0c84ad0b109fa16f3eafc58767c7766f40141
theblackprince89 <tbproy@gmail.com>
2013-09-11 22:57:48 -0500

    reformatted the code to correct indentation added public variable currFileFrameSize to MusicServer.java display frame size and file name on file load in Server.

--- SimplePlayer/src/server/MusicServer.java
+++ SimplePlayer/src/server/MusicServer.java
@@ -10,12 +10,13 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
 
+import util.Mp3Info;
 import javazoom.jl.player.Player;
 
 public class MusicServer {
-	
-	
+
 	int port;
 	File currFile;
 	int currIndex;
@@ -25,73 +26,78 @@ public class MusicServer {
 	FileInputStream fis;
 	ServerSocket ss;
 	ArrayList<BufferedOutputStream> outputStreams;
+	ConcurrentHashMap<String, Long> hostDelayMap;
+	public int currentFileFrameSize;
 	final int MAX_CLIENTS;
-	
-	public MusicServer(String[] playList, int port){
-		
-		//TODO: this can be configured using a config attribute
+
+	public MusicServer(String[] playList, int port) {
+
+		// TODO: this can be configured using a config attribute
 		// max clients.
-		MAX_CLIENTS = 2;
-		
-		
-		if(playList.length <1)
-		{
-			System.out.println("Invalid playlist. Playlist must have atleast one entry");
+		MAX_CLIENTS = 1;
+
+		if (playList.length < 1) {
+			System.out
+					.println("Invalid playlist. Playlist must have atleast one entry");
 			System.exit(1);
-			
-		}
-		else
-		{
+
+		} else {
 			this.playList = new ArrayList<File>();
-			for(String filename : playList){
-				System.out.println("Adding file : " + filename + " to playlist");
+			for (String filename : playList) {
+				System.out
+						.println("Adding file : " + filename + " to playlist");
 				File f = new File(filename);
 				this.playList.add(f);
 			}
 		}
-		
-		this.port  = port;
+
+		this.port = port;
 		currIndex = 0;
 		currFile = this.playList.get(currIndex);
 	}
-	
-	public void startUp()
-	{
+
+	public void startUp() {
 		System.out.println("Starting music server");
 		try {
 			ss = new ServerSocket(port);
 			outputStreams = new ArrayList<BufferedOutputStream>();
-			System.out.println("Server started on port : " + port );
+			System.out.println("Server started on port : " + port);
 			fis = new FileInputStream(currFile);
 			bis = new BufferedInputStream(fis);
-			
-			
+			Mp3Info info = new Mp3Info(currFile.getAbsolutePath());
+			System.out.println("SERVER: file loaded: " + currFile.getName());
+			currentFileFrameSize = (Integer) info.getProperties().get(
+					"mp3.framesize.bytes");
+			System.out.println("SERVER : frame size = " + currentFileFrameSize);
+
 			int count = 0;
-			while(count<MAX_CLIENTS){
+			while (count < MAX_CLIENTS) {
 				Socket s = ss.accept();
 				String remoteHostname = s.getInetAddress().getHostName();
-				System.out.println("Host : " + remoteHostname + " added to listening list");
-				BufferedOutputStream bos = new BufferedOutputStream(s.getOutputStream());
+				System.out.println("Host : " + remoteHostname
+						+ " added to listening list");
+				BufferedOutputStream bos = new BufferedOutputStream(
+						s.getOutputStream());
 				outputStreams.add(bos);
 				count++;
 			}
-			
+
 			System.out.println("Max number of clients have joined.");
-					
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-	}
-	
-	public void playSong(){
 
+	}
+
+	public void playSong() {
+
+		System.out.println("Starting streaming ...");
 		try {
-			while(bis.available()>0)
-			{
+			while (bis.available() > 0) {
 				int byt = bis.read();
-				for(BufferedOutputStream bos : outputStreams){
+				for (BufferedOutputStream bos : outputStreams) {
 					bos.write(byt);
 				}
 			}
@@ -101,18 +107,16 @@ public class MusicServer {
 		}
 		System.out.println("End of song");
 	}
-	
-	
-	public static void main(String args[]) throws IOException{
-		
-		//TODO: read port number from config file.
+
+	public static void main(String args[]) throws IOException {
+
+		// TODO: read port number from config file.
 		int port = 9999;
 		MusicServer server = new MusicServer(args, port);
 		server.startUp();
-		System.out.println("Press enter to start music multicast.");
-		new BufferedReader(new InputStreamReader(System.in)).readLine();
+		// System.out.println("Press enter to start music multicast.");
+		// new BufferedReader(new InputStreamReader(System.in)).readLine();
 		server.playSong();
 	}
-	
- 
+
 }
