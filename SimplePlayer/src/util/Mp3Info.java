7b02e41751337680d5925559e90f1e6d1cfe157f
theblackprince89 <tbproy@gmail.com>
2013-09-11 20:45:27 -0500

    added ternary operator to avoid NullPointerException if duration is null

--- SimplePlayer/src/util/Mp3Info.java
+++ SimplePlayer/src/util/Mp3Info.java
@@ -63,6 +63,40 @@ public class Mp3Info {
 		
 		
 	}
+	public Mp3Info(File currFile) {
+		try {
+            File file = currFile;
+            this.mp3FileName = currFile.getAbsolutePath() + currFile.getName();
+            System.out.println("getting info for file : " + this.mp3FileName);
+            AudioFileFormat baseFileFormat = AudioSystem.getAudioFileFormat(file);
+            properties = baseFileFormat.properties();
+//            for(Object o : properties.keySet())
+//            {
+//            	System.out.println("property name : " + o.toString() + " : " 
+//            			+ properties.get(o.toString()).toString());
+//            	
+//            }
+            
+            track = properties.get("mp3.id3tag.track").toString();
+        	copyright = (Boolean)properties.get("mp3.copyright");
+        	framesLength = (Integer)properties.get("mp3.length.frames");
+        	album = properties.get("album").toString();
+        	date = properties.get("date").toString();
+        	genre = properties.get("mp3.id3tag.genre").toString();
+        	artist = properties.get("author").toString();
+        	title = properties.get("title").toString();
+        	composer = properties.get("mp3.id3tag.composer").toString();
+        	encoding = properties.get("mp3.version.encoding").toString();
+        	fps = (Float)properties.get("mp3.framerate.fps");
+            duration = new Duration((Long)properties.get("duration"));
+            samplingFrequency = (Integer)properties.get("mp3.frequency.hz");
+        	vbr = (Boolean)properties.get("mp3.vbr");
+        	original = (Boolean)properties.get("mp3.original");
+         }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+	}
 	public String getMp3FileName() {
 		return mp3FileName;
 	}
@@ -122,7 +156,7 @@ public class Mp3Info {
 				+ "\n framesLength = " + framesLength + "\n album = " + album
 				+ "\n date = " + date + "\n genre = " + genre + "\n artist = "  + artist
 				+ "\n title = " + title + "\n composer = " + composer + "\n encoding = "
-				+ encoding + "\n fps = " + fps + "\n duration = " + duration.getHumanReadableForm()
+				+ encoding + "\n fps = " + fps + "\n duration = " + (duration !=null ?duration.getHumanReadableForm():"null")
 				+ "\n samplingFrequency = " + samplingFrequency + "\n vbr = " + vbr
 				+ "\n original = " + original + "\n";
 		}
